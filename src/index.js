c530416783214a0b335f0a3581dd4ccb08be4b1f
Momfus <momfus@outlook.com>
2023-01-21 06:23:41 -0300

    sort filter function added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 
 let cryptoData = [];
+const openPriceSelect = document.getElementById('openPriceSelect');
+const baseAssetSelect = document.getElementById('baseAssetSelect');
 
 $(document).ready( () => {
 
-   
+
    getData();
 
    $('#crypto-table').DataTable({
@@ -26,30 +28,63 @@ $(document).ready( () => {
    })
    // Add a function to add and fix the error to show when the sort column is asc or desc
    .on('draw.dt', function () {
- 
+
       let sortedColumn = $(this).DataTable().order()[0][0];
-    
+      let typeSort = 'asc';
+
       // Remove the icons if others columsn have
       $('th i').remove();
-    
+
       // Add arrow for asc or desc sort
       if ($(this).DataTable().order()[0][1] === 'asc') {
-        $('th:eq(' + sortedColumn + ')').append('<i class="bi bi-arrow-down"></i>');
+         $('th:eq(' + sortedColumn + ')').append('<i class="bi bi-arrow-down"></i>');
       } else {
-        $('th:eq(' + sortedColumn + ')').append('<i class="bi bi-arrow-up"></i>');
+         typeSort = 'desc';
+         $('th:eq(' + sortedColumn + ')').append('<i class="bi bi-arrow-up"></i>');
       }
-    });
 
+      setSortSelectValues(sortedColumn, typeSort);
+   });
+
+   
    // add custom event filters
    $('#searchBar').on('keyup', function() {
       $('#crypto-table').DataTable().search(this.value).draw();
    });
+
+   openPriceSelect.addEventListener('change', sortOpenPrice);
+   baseAssetSelect.addEventListener('change', sortBaseAsset);
 
    // Hide default search
    $('.dataTables_filter').hide();
 
 });
 
+function setSortSelectValues( columnIndex, typeSortString ) {
+   if( columnIndex === 1 ) {
+
+      openPriceSelect.value = 'noSorting';
+      if( typeSortString === 'asc') {
+         baseAssetSelect.value = 'A-Z';
+      } else {
+         baseAssetSelect.value = 'Z-A';
+      }
+
+   } else if( columnIndex === 2 ) {
+
+      baseAssetSelect.value = 'noSorting';
+      if( typeSortString === 'asc') {
+         openPriceSelect.value = 'lowToHigh';
+      } else {
+         openPriceSelect.value = 'highToLow';
+      }
+
+   } else {
+
+      baseAssetSelect.value = 'noSorting';
+      openPriceSelect.value = 'noSorting';
+   }
+}
 
 function showHideLoadData( show ) {
 
@@ -70,8 +105,8 @@ function getData() {
    showHideLoadData(false);
 
    $.ajax({
-      url: "https://api.wazirx.com/sapi/v1/tickers/24hr",
-      method: "GET",
+      url: 'https://api.wazirx.com/sapi/v1/tickers/24hr',
+      method: 'GET',
       success: (data) => {
          console.log(data); // TODO delete
 
@@ -85,5 +120,62 @@ function getData() {
          console.error(error);
       }
    });
+}
+
+
+function sortOpenPrice() {
+
+   const selectedValue = openPriceSelect.value;
+   let orderType;
+
+   switch( selectedValue ) {
+      
+      case 'lowToHigh': {
+         orderType = 'asc';
+         break;
+      }
+      
+      case 'highToLow': {
+         orderType = 'desc';
+         break;
+      }
+
+      default: {
+         $('#crypto-table').DataTable().order([0, 'asc']).draw();
+         return;
+      }
+
+   }
+
+   baseAssetSelect.value = 'noSorting';
+   $('#crypto-table').DataTable().order([2, orderType]).draw();
+}
+
+function sortBaseAsset() {
+
+   const selectedValue = baseAssetSelect.value;
+   let orderType;
+
+   switch(selectedValue) {
+
+      case 'A-Z': {
+         orderType = 'asc';
+         break;
+      }
+
+      case 'Z-A': {
+         orderType = 'desc';
+         break;
+      }
+
+      default: {
+         $('#crypto-table').DataTable().order([0, 'asc']).draw();
+         return;
+      }
+
+   }
+
+   openPriceSelect.value = 'noSorting';
+   $('#crypto-table').DataTable().order([1, orderType]).draw();
 }
 
