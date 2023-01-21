00da3dfbe611bac3e78a47f1bb7aec14c31f9b72
Momfus <momfus@outlook.com>
2023-01-20 22:57:43 -0300

    logic done

--- README.md
+++ README.md
@@ -5,7 +5,14 @@ An small tech challenge to create a crypto currency list with javascript, jquery
 
 * npm install
 * npm run dev
- 
+* Go to http://localhost:5173/ (or the one with the port that the console show)
+
+
+## NOTES:
+
+* The API documentation is here: https://docs.wazirx.com/#24hr-tickers-price-change-statistics
+* The API doesnt have a "limit/skip" or some way to manage the pagination from the server instead of the client (in that case, is much easy applying a lazy load)
+
 ## Challenge
 
 In this challenge, we want to show a list with 10 crypto currency for page, in each row, should be displayed: symbol, base asset, open price, low price, high price, last price and volume. 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,96 @@
-function test() {
-   console.log('hi');
+
+
+let cryptoData = [];
+let cryptoTable = '';
+
+$(document).ready( () => {
+
+   setupTable();
+   getData();
+});
+
+function setupTable() {
+   cryptoTable = $('#crypto-table').DataTable({
+      data: cryptoData,
+      columns: [
+          { data: 'symbol' },
+          { data: 'baseAsset' },
+          { data: 'openPrice' },
+          { data: 'lowPrice' },
+          { data: 'highPrice' },
+          { data: 'lastPrice' },
+          { data: 'volume' }
+      ],
+      "order": [[ 1, "asc" ]],
+  });
 }
 
+function showHideLoadData( show, isDataFound ) {
 
-test();
+   if( isDataFound ) {
+
+      $('#noDataFound').hide();
+
+      if( show ) {
+         $('#isDataLoading').hide();
+         $('#isDataLoaded').show();
+      } else {
+         $('#isDataLoading').show();
+         $('#isDataLoaded').hide();
+      }
+
+   } else {
+      $('#isDataLoading').hide();
+      $('#isDataLoaded').hide();
+
+      $('#noDataFound').show();
+
+   }
+      
+}
+
+function getData() {
+
+   showHideLoadData(false, true);
+
+   $.ajax({
+      url: "https://api.wazirx.com/sapi/v1/tickers/24hr",
+      method: "GET",
+      success: (data) => {
+        console.log(data); // TODO delete
+  
+        cryptoData = data;
+        cryptoTable.clear();
+        cryptoTable.rows.add(data);
+        cryptoTable.draw();
+
+        showHideLoadData(true, cryptoData.length > 0);
+  
+      },
+      error: function(error) {
+        console.error(error);
+      }
+    });
+}
+
+function setupFilters() {
+   
+   $('#base-filter').on('keyup', function() {
+      cryptoTable.column(1).search(this.value).draw();
+   });
+
+   $('#sort-filter').on('change', function() {
+      cryptoTable
+            .order([2, this.value])
+            .draw();
+   });
+
+   $('#order-filter').on('change', function() {
+      var column = cryptoTable.order()[0][0];
+      cryptoTable
+            .order([column, this.value])
+            .draw();
+   });
+
+}
+
